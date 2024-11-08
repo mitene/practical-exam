80fb6563426436c7e06faa6941a5cf3b2286df78
Satoru Sasaki <satoru.sasaki@mixi.co.jp>
2024-11-08 14:31:10 +0900

    update compile sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "us.mitene.practicalexam"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "us.mitene.practicalexam"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
